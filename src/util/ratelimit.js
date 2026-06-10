877c6c63d9b06a726e7e2555857799cb5389b0e2
LordCat <171042544+LordCat0@users.noreply.github.com>
2026-06-10 12:44:55 -0400

    Revert "feat: send ratelimits to webhook"
    This reverts commit 8e74ca1fd35a9dbe95749887146dd04d2140cef4.

--- src/util/ratelimit.js
+++ src/util/ratelimit.js
@@ -1,26 +1,6 @@
-import crypto from 'crypto';
 import rateLimit from 'express-rate-limit';
-import webhookService from '../services/webhook.js';
 
 export default rateLimit({
     windowMs: 5 * 60 * 1000, // 5 Minutes
     limit: 50,
-    handler: (req, res) => {
-        const hash = crypto
-            .createHash('sha256')
-            .update(req.ip)
-            .digest('base64');
-
-        webhookService.send({
-            embeds: [
-                {
-                    title: 'Request ratelimit',
-                    description: `IP hash: ${hash}`,
-                    color: 0xff0000,
-                },
-            ],
-        });
-
-        res.sendStatus(429);
-    },
 });
